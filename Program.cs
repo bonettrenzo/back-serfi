89da849863b43ce4e7c3ef5ddc90d2169bd91cea
Renzo Bonett <bonettrenzo@gmail.com>
2025-06-10 17:02:16 -0500

    Update Program.cs
    ajuste en la manera en la que se devuelven los jsons

--- Program.cs
+++ Program.cs
@@ -20,8 +20,10 @@ builder.Services.AddCors(options  =>
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
-    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.WriteIndented = true; 
 });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
